327fa5b2cfff642737aa4424ba92f782434b7d43
arabou <abdelfattah.rabou@capgemini.com>
2024-04-13 16:55:26 +0000

    Setting inProgress = true when creating a new tournament

--- tournoi/src/main/java/com/capgemini/tournoi/repos/TournamentRepository.java
+++ tournoi/src/main/java/com/capgemini/tournoi/repos/TournamentRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TournamentRepository extends JpaRepository<Tournament, Long> {
 
-    @Query(value = "select * from tournament t where t.inProgress=true",nativeQuery = true)
+    @Query(value = "select * from tournament t where t.in_progress=true",nativeQuery = true)
     public Tournament checkExistTournamentInProgress();
 }
